780c8fc0ba8ac5c1645f5be0d004889d0da843e9
Alexey Zimin <aazimin101@gmail.com>
2025-03-21 06:19:18 +0300

    fix comment commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
 const cardsList = document.querySelector('.places__list');
-
 
 // @todo: Функция создания карточки
 function createCard(description, link, deleteFunc){
@@ -10,26 +10,25 @@ function createCard(description, link, deleteFunc){
     // cardElement.querySelector('card__image').src = 'tinyurl.com/v4pfzwy';
     const cardImage = cardElement.querySelector('.card__image');
     cardImage.src = link;
+    cardImage.alt = description;
     
     const cardTitle = cardElement.querySelector('.card__title');
     cardTitle.textContent = description;
     
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', deleteFunc);
-
-    cardsList.append(cardElement);
+    deleteButton.addEventListener('click', ()=>deleteCard(cardElement));
 
     return cardElement;
 }
+
 // @todo: Функция удаления карточки
-function deleteCard(evt){
-    const card = evt.target.closest('.card');
-    card.remove();
+function deleteCard(cardElement){
+    cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
 function createPageItems(){
-    initialCards.forEach(item=>createCard(item.name, item.link, deleteCard));
+    initialCards.forEach(item=>cardsList.append(createCard(item.name, item.link, deleteCard)));
 }
 
 createPageItems();
