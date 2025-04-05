15326667f6c94adac39686a01283686b02dddd13
Alexey Zimin <aazimin101@gmail.com>
2025-04-05 14:58:12 +0300

    setup webpack, add .gitignore

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
+import '../pages/index.css';
+// import initialCards from "./../scripts/cards.js";
+import {initialCards} from "./cards.js"
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
